f26f778b0ad0c08d3945682d063113983c13720a
ayz6uem <360188606@qq.com>
2018-10-18 16:15:26 +0800

    :pig: 又重命名

--- src/main/java/com/github/apiggy/visitor/NodeVisitor.java
+++
@@ -1,7 +0,0 @@
-package com.github.apiggy.visitor;
-
-import com.github.apiggy.schema.Node;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-
-public abstract class NodeVisitor extends VoidVisitorAdapter<Node> {
-}
